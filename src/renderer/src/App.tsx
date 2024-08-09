733cff9041de1c6b91ebf5248834ffc8e528d3ec
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-08-09 19:08:49 +0530

    create actionbuttonrow for file actions

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,11 @@
-import { Content, RootLayout,SideBar, DraggableTopBar } from "@/components"
+import { Content, RootLayout,SideBar, DraggableTopBar,ActionButtonsRow } from "@/components"
 const App=()=>{
   return (
     <>
       <DraggableTopBar/>
     <RootLayout className="bg-black bg-opacity-80">
       <SideBar  className="p-2">
-        sidebar
+        <ActionButtonsRow className="flex justify-between mt-1"/>
       </SideBar>
       <Content className="border-l border-zinc-900/50 border-l-white/20">
         content 
