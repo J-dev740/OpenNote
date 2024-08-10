a0a574755b17636d96230e1a63c2f668a776791f
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-08-10 23:43:10 +0530

    create createNote handler

--- src/main/lib/index.ts
+++ src/main/lib/index.ts
@@ -1,8 +1,10 @@
 import { appDirectoryName, fileEncoding } from "@shared/constants"
 import { NoteInfo } from "@shared/models"
-import { GetNotes, ReadNote, WriteNote } from "@shared/types"
+import { CreateNote, GetNotes, ReadNote, WriteNote } from "@shared/types"
+import { dialog } from "electron"
 import { ensureDir,readdir, readFile, stat, writeFile } from "fs-extra"
 import { homedir } from "os"
+import path from "path"
 
 export const getRootDir=()=>{
     return `${homedir()}/${appDirectoryName}`
@@ -36,4 +38,37 @@ export const writeNote:WriteNote=async(filename,content)=>{
     const rootDir=getRootDir()
     console.info(`writing note ${filename}`);
     return writeFile(`${rootDir}/${filename}.md`,content,{encoding:fileEncoding});
+}
+
+export const createNote:CreateNote=async()=>{
+    const rootDir=getRootDir()
+    await ensureDir(rootDir);
+    // when creating a new note we need to display the user info about what should be the title  and folder of the note 
+    const{filePath,canceled}= await dialog.showSaveDialog({
+        title:'New Note',
+        defaultPath:`${rootDir}/Untitled.md`,
+        buttonLabel:'Create',
+        properties:['showOverwriteConfirmation'],
+        showsTagField:false,
+        filters:[{
+            name:'Markdown',extensions:['md']
+        }]
+    })
+    if(canceled || !filePath){
+        console.info('note creation canceled');
+        return false;
+    }
+    const {name:filename,dir:parentDir}=path.parse(filePath)
+    if(parentDir!==rootDir){
+        await dialog.showMessageBox({
+            type:'error',
+            title:'creation failed',
+            message:`All notes must be saved under ${rootDir}, Avoid using other dirs!`
+
+        })
+        return false;
+    }
+    console.info(`creating note ${filePath}`)
+    await writeFile(filePath,'',{encoding:fileEncoding})
+    return filename
 }
