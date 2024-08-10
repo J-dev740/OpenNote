99f5e8384fec2f57e83947bcf606debf217ed738
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-08-10 23:18:22 +0530

    implemented handle for autosaving

--- src/renderer/src/hooks/useMarkDownEditor.tsx
+++ src/renderer/src/hooks/useMarkDownEditor.tsx
@@ -1,9 +1,21 @@
-import { selectedNoteAtom } from "@renderer/store"
-import { useAtomValue } from "jotai"
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import { saveNoteAtom, selectedNoteAtom } from "@renderer/store"
+import { NoteContent } from "@shared/models";
+import { useAtomValue, useSetAtom } from "jotai"
+import { useRef } from "react";
 
 export const useMarkDownEditor=()=>{
     const selectedNote=useAtomValue(selectedNoteAtom);
+    const saveNote=useSetAtom(saveNoteAtom)
+    const editorRef=useRef<MDXEditorMethods>(null)
+    const handleAutoSaving=async (content:NoteContent)=>{
+        if(!selectedNote) return ;
+        console.info('auto saving:',selectedNote.title);
+        await saveNote(content)
+    }
     return{
-        selectedNote
+        selectedNote,
+        editorRef,
+        handleAutoSaving
     }
 }
